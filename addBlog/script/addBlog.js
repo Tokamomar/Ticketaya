271dfa70c7ef6be1c48b2b281f9de863c5467166
mariemomar <113386463+mariemomar@users.noreply.github.com>
2024-09-07 12:51:34 +0300

    blog posts page

--- addBlog/script/addBlog.js
+++ addBlog/script/addBlog.js
@@ -1,12 +1,17 @@
-//! this is an alert 
-//todo  
-//? this is for questions 
+
 
 const toggleOptions = document.querySelectorAll('.toggle-option');
-
 toggleOptions.forEach(option => {
   option.addEventListener('click', function() {
     toggleOptions.forEach(opt => opt.classList.remove('active'));
     this.classList.add('active');
   });
 });
+
+const backBtn = document.getElementById('back');
+backBtn.addEventListener('click' , ()=>{
+  window.location.href = '../../addBlog/index/blogPosts.html';
+})
+
+//TODO make the img required
+//!  DONT forget the time 
---
+++ addBlog/script/blogPosts.js
@@ -0,0 +1,8 @@
+const logout_btn = document.getElementById('logout_btn');
+logout_btn.addEventListener('click', ()=>{
+    window.location.href = '../../Registration/index/login.html'
+});
+
+//todo click heart -> red
+//todo click comment  -> make the mouse on the input for comments
+//todo click view all comments -> open the page of all comments
