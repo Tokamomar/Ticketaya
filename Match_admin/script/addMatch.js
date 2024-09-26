af0532c5b2ffdd3903fd34e5973e55bc2d7d0918
Toka Mohamed <111085772+Tokamomar@users.noreply.github.com>
2024-09-26 21:43:49 +0300

    doneeeeeee

--- Match_admin/script/addMatch.js
+++ Match_admin/script/addMatch.js
@@ -1,9 +1,8 @@
 document.getElementById('addMatchForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    // Fetch the accessToken for authorization
     const token = localStorage.getItem('accessToken');
-    
+
     const name = document.getElementById('matchName').value;
     const team1 = document.getElementById('team1').value;
     const team2 = document.getElementById('team2').value;
@@ -13,26 +12,26 @@ document.getElementById('addMatchForm').addEventListener('submit', function(even
     const no_tickets = document.getElementById('ticketCount').value;  
     const ticket_price = document.getElementById('ticketPrice').value;
     const description = document.getElementById('description').value;
+    const imageFile = document.getElementById('matchImage').files[0]; 
 
-    const matchData = {
-        name: name,
-        team1: team1,
-        team2: team2,
-        date: date,
-        time: time,
-        stadium: stadium,
-        no_tickets: no_tickets,
-        ticket_price: ticket_price,
-        description: description
-    };
+    const matchData = new FormData();
+    matchData.append('name', name);
+    matchData.append('team1', team1);
+    matchData.append('team2', team2);
+    matchData.append('date', date);
+    matchData.append('time', time);
+    matchData.append('stadium', stadium);
+    matchData.append('no_tickets', no_tickets);
+    matchData.append('ticket_price', ticket_price);
+    matchData.append('description', description);
+    matchData.append('image', imageFile); 
 
     fetch('http://127.0.0.1:8000/match/addnewmatch/', {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`
+            'Authorization': `Bearer ${token}` 
         },
-        body: JSON.stringify(matchData)
+        body: matchData 
     })
     .then(response => {
         if (!response.ok) {
@@ -40,10 +39,14 @@ document.getElementById('addMatchForm').addEventListener('submit', function(even
         }
         return response.json();
     })
+
     .then(data => {
-        // Show success message and redirect to dashboard
-        alert('New match added successfully!');
-        window.location.href = 'matchManagement.html'; 
+        document.getElementById('overlay').style.display = 'block';
+        document.getElementById('successMessage').style.display = 'block';
+
+        setTimeout(() => {
+            window.location.href = 'matchManagement.html';
+        }, 2000);
     })
     .catch(error => {
         console.error('Error:', error);
