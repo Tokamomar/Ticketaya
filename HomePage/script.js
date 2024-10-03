7c64e159b6c3551b09ad4fc2781d8459fc73156d
Toka Mohamed <111085772+Tokamomar@users.noreply.github.com>
2024-10-03 18:12:21 +0300

    home page

--- HomePage/script.js
+++ HomePage/script.js
@@ -1,15 +1,28 @@
-// JavaScript to automatically change the background image of the landing section
-
 const landing = document.querySelector('.landing');
 
 // Array of background images
 const backgrounds = [
     "url('images/slider1.jpg')",
     "url('images/slider2.jpg')",
-    "url('images/slider3.jpg')"
+    "url('images/slider3.jpg')",
+    "url('images/slider4.jpg')"
 ];
 
 let currentIndex = 0;
+
+// Preload images
+function preloadImages(urls) {
+    urls.forEach((url) => {
+        const img = new Image();
+        img.src = url;
+    });
+}
+preloadImages([
+    'images/slider1.jpg',
+    'images/slider2.jpg',
+    'images/slider3.jpg',
+    'images/slider4.jpg'
+]);
 
 // Function to change background image
 function changeBackground() {
@@ -17,9 +30,13 @@ function changeBackground() {
     landing.style.backgroundImage = backgrounds[currentIndex];
 }
 
+// Set initial background image
+landing.style.backgroundImage = backgrounds[currentIndex];
+
 // Set interval to change the background every 5 seconds
 setInterval(changeBackground, 5000);
-// Get elements
+
+// Get elements for the popup
 const openAbout = document.getElementById('openAbout');
 const popup = document.getElementById('aboutUsPopup');
 const closeBtn = document.querySelector('.close-btn');
